36b91b6b19a4828238383d6373c2d709c0115321
Rajeev Sharma <rajeevsharma86@gmail.com>
2019-03-14 20:27:10 +0530

    Updates

---
+++ precache-manifest.782a34ef9c0dad10769e1bfc340dd520.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3b0b39aeac5a6c5cfc4c",
+    "url": "/season/static/js/runtime~main.580e902c.js"
+  },
+  {
+    "revision": "2ea8a9b7c89cc2e7b63d",
+    "url": "/season/static/js/main.a43de17a.chunk.js"
+  },
+  {
+    "revision": "49e8ed93f0c971c12a69",
+    "url": "/season/static/js/2.11b5b539.chunk.js"
+  },
+  {
+    "revision": "2ea8a9b7c89cc2e7b63d",
+    "url": "/season/static/css/main.8b7323df.chunk.css"
+  },
+  {
+    "revision": "06fadf8fb87c6b996dffd639c69ef24d",
+    "url": "/season/index.html"
+  }
+];
